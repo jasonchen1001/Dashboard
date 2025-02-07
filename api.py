c9145b3d7e726d4ac8fb17b97dae426d1d35c290
yanzhenchen <chen.yanzh@northeastern.edu>
2025-02-07 12:36:16 -0500

    add

--- api.py
+++ api.py
@@ -2,14 +2,11 @@ import pandas as pd
 
 class DeliveryAPI:
     def __init__(self, filepath):
-        """初始化 API"""
         self.data = pd.read_csv(filepath)
     
     def get_data_by_filters(self, agent=None, location=None, order_type=None, rating_range=None):
-        """根据条件获取数据"""
         filtered_data = self.data.copy()
         
-        # 应用过滤条件
         if agent and agent != 'All':
             filtered_data = filtered_data[filtered_data['Agent Name'] == agent]
             
